55e72f0134fb8927e7b8524856d55cccdf614937
Ahmed Emam <aemam@pixel.com.kw>
2023-07-12 18:19:53 +0300

    Featured book item

--- lib/features/home/presentation/widgets/featured_book_item.dart
+++ lib/features/home/presentation/widgets/featured_book_item.dart
@@ -1,21 +1,6 @@
-import 'package:bookly_mvvm/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-import 'custom_app_bar.dart';
-
-class HomeViewBody extends StatelessWidget {
-  const HomeViewBody({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: const [
-        CutomAppBar(),
-        FeaturedBookItem(),
-      ],
-    );
-  }
-}
+import '../../../../core/utils/assets.dart';
 
 class FeaturedBookItem extends StatelessWidget {
   const FeaturedBookItem({super.key});
@@ -25,6 +10,7 @@ class FeaturedBookItem extends StatelessWidget {
     return SizedBox(
       height: MediaQuery.of(context).size.height * .22,
       child: AspectRatio(
+        // ==>Width /height
         aspectRatio: 2.5 / 4,
         child: Container(
           decoration: BoxDecoration(
