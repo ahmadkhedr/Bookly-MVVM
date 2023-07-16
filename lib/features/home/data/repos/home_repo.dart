0078f106b635da82b49ecb2bcd306030fd685958
Ahmed Emam <aemam@pixel.com.kw>
2023-07-16 16:56:53 +0300

    Failure class ,Home repo Edits

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,7 @@
+import 'package:bookly_mvvm/features/home/data/models/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  featchFeaturedBooks();
+  Future<Either<BookModel, BookModel>> featchFeaturedBooks();
   featchBesetSellerBooks();
 }
